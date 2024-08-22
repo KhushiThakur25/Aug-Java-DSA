ac46a1c663326fbef3531068f252a9fe2ffeb025
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-08-22 21:33:03 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    static void print_name(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println("Riya");
+        print_name(n - 1);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        print_name(n);
+    }
+}
