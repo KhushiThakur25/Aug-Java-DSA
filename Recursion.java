780b13e78003f1b6f54c796edb2e87dafcfaa5ff
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-08-22 21:39:57 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,13 +1,17 @@
 public class Recursion {
     static void print_name(int n){
+        //Base Case or Termination Case 
         if(n == 0){
             return;
         }
+        //Logic For printing Name
         System.out.println("Riya");
+        //Recursive Call with Small Problem
         print_name(n - 1);
     }
     public static void main(String[] args) {
         int n = 5;
+        //Initial Call 
         print_name(n);
     }
 }
